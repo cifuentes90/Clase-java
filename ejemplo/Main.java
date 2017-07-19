994fed12c9099d27901528d294ac0f22c0a17e8c
karen.sebas1025 <karen.sebas1025@gmail.com>
2017-07-19 12:09:12 -0400

    suma, resta, multiplicacion, division

--- ejemplo/Main.java
+++ ejemplo/Main.java
@@ -3,13 +3,51 @@ package ejemplo;
 public class Main{
   
   public static void main (String[]args){
+    
     System.out.println("hola amigos");
+    
     int s = suma(20, 30);
+    
     System.out.println(s);
+    
+    int r = resta(20, 10);
+    
+    System.out.println(r);
+    
+    int m = multiplicacion(2, 3);
+    
+    System.out.println(m);
+      
+    try {
+
+      double d = division(10, 0);
+      System.out.println(d);
+    }  catch(Exception e){
+      String mensaje = e.getMessage();
+      System.out.println(mensaje);
+    }
   }
   
   public static int suma(int n1, int n2) {
     return n1 + n2;
   }
+  
+  public static int resta(int n1, int n2) {
+    return n1 - n2;
+  }
+  
+  public static int multiplicacion(int n1, int n2) {
+    return n1 * n2;
+    
+  }
+  
+  public static double division(double n1, int n2) throws Exception {
+    if (n2 == 0) {
+      throw new  Exception("no es posible hacer division por cero");
+    }
+    return n1 / n2;
+    
+  } 
+   
   
 }
